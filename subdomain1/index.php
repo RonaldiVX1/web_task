ad3bebd90f94dadd8b0b204b05f43b90db846dd6
Ronaldi <87842194+RonaldiVX1@users.noreply.github.com>
2022-04-11 11:58:16 +0700

    commit

---
+++ subdomain1/index.php
@@ -0,0 +1,179 @@
+<!-- 
+<!DOCTYPE html>
+<link rel="stylesheet" href="../theme/theme.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-5">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <div class="search-container">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Submit</button>
+  </div>
+</div>
+
+<img src="../theme/banner.jpg" alt="Cinque Terre" width="1520" height="600">
+<div class="gallery">
+
+
+</div>
+
+
+<div class="row-gallery">
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_5terre.jpg">
+      <img src="../theme/product1.jpg" alt="Cinque Terre" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_forest.jpg">
+      <img src="../theme/product1.jpg" alt="Forest" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_forest.jpg">
+      <img src="../theme/product1.jpg" alt="Forest" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+</div>
+
+
+<div class="row-gallery">
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_lights.jpg">
+      <img src="../theme/product1.jpg" alt="Northern Lights" width="200" height="100">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_lights.jpg">
+      <img src="../theme/product1.jpg" alt="Northern Lights" width="200" height="100">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+</div>
+
+
+<footer>
+<p>© 2022 TOKO GUNDAM SUKA MAJU</p>
+</footer>
+
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<link rel="stylesheet" href="../theme/theme.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav> <div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#news">About</a>
+  <div class="dropdown">
+    <button class="dropbtn">Product
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">GUNPLA</a>
+      <a href="#">ACTION FIGURE</a>
+      <a href="#">LEGO</a>
+    </div>
+  </div>
+</div>
+</nav>
+
+<div class="container">
+  <img src="../theme/banner2.jpg" alt="Snow" style="width:100%;">
+  <div class="top-left">
+    <h1>Gundam Wings</h1>
+    <h1>RX-72 </h1>
+    <button class="button button1">Order Now</button>
+</div>
+</div>
+
+
+<div class="column">
+<div class="row">
+<div class="card">
+  <img src="../theme/product1.jpg" alt="Avatar" style="width: 100%">
+  <div class="card-detail">
+    <h4><b>John Doe</b></h4> 
+    <p>Architect & Engineer</p> 
+  </div>
+</div>
+
+<div class="card">
+  <img src="../theme/product1.jpg" alt="Avatar" style="width: 100%">
+  <div class="card-detail">
+    <h4><b>John Doe</b></h4> 
+    <p>Architect & Engineer</p> 
+  </div>
+</div>
+
+<div class="card">
+  <img src="../theme/product1.jpg" alt="Avatar" style="width: 100%">
+  <div class="card-detail">
+    <h4><b>John Doe</b></h4> 
+    <p>Architect & Engineer</p> 
+  </div>
+</div>
+</div>
+
+
+<div class="row">
+<div class="card">
+  <img src="../theme/product1.jpg" alt="Avatar" style="width: 100%">
+  <div class="card-detail">
+    <h4><b>John Doe</b></h4> 
+    <p>Architect & Engineer</p> 
+  </div>
+
+  
+</div>
+
+<div class="card">
+  <img src="../theme/product1.jpg" alt="Avatar" style="width: 100%">
+  <div class="card-detail">
+    <h4><b>John Doe</b></h4> 
+    <p>Architect & Engineer</p> 
+  </div>
+</div>
+
+</div>
+    
+</body>
+</html>
